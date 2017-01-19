09b2f79425f12bb5bcba9d35c846411c5b336965
Drake <drake.olson@jacks.sdstate.edu>
2017-01-19 00:40:24 -0600

    Insights Updated on Each View Creation

--- BasicLayout.Designer.cs
+++ BasicLayout.Designer.cs
@@ -43,8 +43,8 @@
             this.removeItemToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.reportsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.flowLayoutPanel1 = new System.Windows.Forms.FlowLayoutPanel();
-            this.button1 = new System.Windows.Forms.Button();
-            this.button2 = new System.Windows.Forms.Button();
+            this.todayTotalPatronInsightButton = new System.Windows.Forms.Button();
+            this.todayNewPatronInsightButton = new System.Windows.Forms.Button();
             this.titleOfDataLabel = new System.Windows.Forms.Label();
             this.dataGridView1 = new System.Windows.Forms.DataGridView();
             this.dashboardMenuStrip.SuspendLayout();
@@ -167,32 +167,32 @@
             // 
             // flowLayoutPanel1
             // 
-            this.flowLayoutPanel1.Controls.Add(this.button1);
-            this.flowLayoutPanel1.Controls.Add(this.button2);
+            this.flowLayoutPanel1.Controls.Add(this.todayTotalPatronInsightButton);
+            this.flowLayoutPanel1.Controls.Add(this.todayNewPatronInsightButton);
             this.flowLayoutPanel1.Location = new System.Drawing.Point(12, 27);
             this.flowLayoutPanel1.Name = "flowLayoutPanel1";
             this.flowLayoutPanel1.Size = new System.Drawing.Size(1252, 73);
             this.flowLayoutPanel1.TabIndex = 12;
             // 
-            // button1
+            // todayTotalPatronInsightButton
             // 
-            this.button1.Location = new System.Drawing.Point(450, 25);
-            this.button1.Margin = new System.Windows.Forms.Padding(450, 25, 3, 3);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(145, 27);
-            this.button1.TabIndex = 0;
-            this.button1.Text = "Today\'s Patrons: 100";
-            this.button1.UseVisualStyleBackColor = true;
+            this.todayTotalPatronInsightButton.Location = new System.Drawing.Point(450, 25);
+            this.todayTotalPatronInsightButton.Margin = new System.Windows.Forms.Padding(450, 25, 3, 3);
+            this.todayTotalPatronInsightButton.Name = "todayTotalPatronInsightButton";
+            this.todayTotalPatronInsightButton.Size = new System.Drawing.Size(145, 27);
+            this.todayTotalPatronInsightButton.TabIndex = 0;
+            this.todayTotalPatronInsightButton.Text = "Today\'s Patrons: 100";
+            this.todayTotalPatronInsightButton.UseVisualStyleBackColor = true;
             // 
-            // button2
+            // todayNewPatronInsightButton
             // 
-            this.button2.Location = new System.Drawing.Point(638, 25);
-            this.button2.Margin = new System.Windows.Forms.Padding(40, 25, 3, 3);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(145, 27);
-            this.button2.TabIndex = 1;
-            this.button2.Text = "Today\'s New Patrons: 100";
-            this.button2.UseVisualStyleBackColor = true;
+            this.todayNewPatronInsightButton.Location = new System.Drawing.Point(638, 25);
+            this.todayNewPatronInsightButton.Margin = new System.Windows.Forms.Padding(40, 25, 3, 3);
+            this.todayNewPatronInsightButton.Name = "todayNewPatronInsightButton";
+            this.todayNewPatronInsightButton.Size = new System.Drawing.Size(145, 27);
+            this.todayNewPatronInsightButton.TabIndex = 1;
+            this.todayNewPatronInsightButton.Text = "Today\'s New Patrons: 100";
+            this.todayNewPatronInsightButton.UseVisualStyleBackColor = true;
             // 
             // titleOfDataLabel
             // 
@@ -250,8 +250,8 @@
         private System.Windows.Forms.ToolStripMenuItem reportsToolStripMenuItem;
         private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel1;
         public System.Windows.Forms.Label titleOfDataLabel;
-        public System.Windows.Forms.Button button1;
-        public System.Windows.Forms.Button button2;
+        public System.Windows.Forms.Button todayTotalPatronInsightButton;
+        public System.Windows.Forms.Button todayNewPatronInsightButton;
         public System.Windows.Forms.DataGridView dataGridView1;
     }
 }
--- Dashboard.cs
+++ Dashboard.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Data.SqlClient;
 
 namespace SDSU_Rock_Wall_CRM
 {
@@ -15,6 +16,52 @@ namespace SDSU_Rock_Wall_CRM
         public Dashboard()
         {
             InitializeComponent();
+            var todayTotalUserInsightPrefix = "Today's Patrons: ";
+            var todayTotalNewUserInsightPrefix = "Today's New Patrons: ";
+            var dateOfToday = DateTime.Today.ToShortDateString();
+            StringBuilder todayTotalUserInsightText = new StringBuilder();
+            string connectionString = "Data Source = sqlpiedpiper.database.windows.net; Initial Catalog=RockWallSDSU; Persist Security Info = True; User ID = devadmin; Password = P@ssword";
+            try
+            {
+                SqlConnection con = new SqlConnection(connectionString);
+                SqlCommand command = new SqlCommand("Select * from Patrons where lastCheckIn=@dateOfToday", con);
+                command.Parameters.AddWithValue("@dateOfToday", dateOfToday);
+                con.Open();
+                SqlDataAdapter adapt = new SqlDataAdapter(command);
+                DataSet returnedData = new DataSet();
+                adapt.Fill(returnedData);
+                if(returnedData.Tables[0].Rows.Count <= 0)
+                {
+                    todayTotalUserInsightText.Append($"{todayTotalUserInsightPrefix} 0");
+                }
+                else
+                {
+                    todayTotalUserInsightText.Append($"{todayTotalUserInsightPrefix} {returnedData.Tables[0].Rows.Count}");
+                }
+                this.todayTotalPatronInsightButton.Text = todayTotalUserInsightText.ToString();
+                todayTotalUserInsightText.Clear();
+
+                command.Parameters.Clear();
+                command.CommandText = "Select * from Patrons where dateOfCreation=@dateOfToday";
+                command.Parameters.AddWithValue("@dateOfToday", dateOfToday);
+                adapt.SelectCommand = command;
+                returnedData.Reset();
+                adapt.Fill(returnedData);
+                if (returnedData.Tables[0].Rows.Count <= 0)
+                {
+                    todayTotalUserInsightText.Append($"{todayTotalNewUserInsightPrefix} 0");
+                }
+                else
+                {
+                    todayTotalUserInsightText.Append($"{todayTotalNewUserInsightPrefix} {returnedData.Tables[0].Rows.Count}");
+                }
+                this.todayNewPatronInsightButton.Text = todayTotalUserInsightText.ToString();
+                con.Close();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
         }
         private void Dashboard_Load(object sender, EventArgs e)
         {
@@ -38,9 +85,6 @@ namespace SDSU_Rock_Wall_CRM
             dashBoard.Location = this.Location;
             dashBoard.StartPosition = this.StartPosition;
             dashBoard.WindowState = FormWindowState.Normal;
-            var todayTotalUserInsightPrefix = "Today's Patrons: ";
-            var dateOfToday = DateTime.Today;
-
             dashBoard.Show();
             this.Hide();
         }
