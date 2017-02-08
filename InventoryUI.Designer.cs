24742d775a54f84606a3038254997e0572b22136
Drake <drake.olson@jacks.sdstate.edu>
2017-02-08 17:06:57 -0600

    Inventory Feature Ready to Test

---
+++ InventoryUI.Designer.cs
@@ -0,0 +1,88 @@
+﻿namespace SDSU_Rock_Wall_CRM
+{
+    partial class InventoryUI
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.inventoryUpdateButton = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // titleOfDataLabel
+            // 
+            this.titleOfDataLabel.Size = new System.Drawing.Size(118, 29);
+            this.titleOfDataLabel.Text = "Inventory";
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dataGridView1.AutoSizeRowsMode = System.Windows.Forms.DataGridViewAutoSizeRowsMode.DisplayedHeaders;
+            this.dataGridView1.BackgroundColor = System.Drawing.SystemColors.Control;
+            this.dataGridView1.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(24, 175);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.dataGridView1.Size = new System.Drawing.Size(1213, 494);
+            this.dataGridView1.TabIndex = 15;
+            // 
+            // inventoryUpdateButton
+            // 
+            this.inventoryUpdateButton.Location = new System.Drawing.Point(1132, 146);
+            this.inventoryUpdateButton.Name = "inventoryUpdateButton";
+            this.inventoryUpdateButton.Size = new System.Drawing.Size(105, 23);
+            this.inventoryUpdateButton.TabIndex = 16;
+            this.inventoryUpdateButton.Text = "Update Inventory";
+            this.inventoryUpdateButton.UseVisualStyleBackColor = true;
+            this.inventoryUpdateButton.Click += new System.EventHandler(this.inventoryUpdateButton_Click);
+            // 
+            // InventoryUI
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1264, 681);
+            this.Controls.Add(this.inventoryUpdateButton);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "InventoryUI";
+            this.Text = "InventoryUI";
+            this.Load += new System.EventHandler(this.InventoryUI_Load);
+            this.Controls.SetChildIndex(this.dataGridView1, 0);
+            this.Controls.SetChildIndex(this.inventoryUpdateButton, 0);
+            this.Controls.SetChildIndex(this.titleOfDataLabel, 0);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button inventoryUpdateButton;
+    }
+}
---
+++ InventoryUI.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace SDSU_Rock_Wall_CRM
+{
+    public partial class InventoryUI : BasicLayout
+    {
+        public InventoryUI()
+        {
+            InitializeComponent();
+        }
+
+        private void InventoryUI_Load(object sender, EventArgs e)
+        {
+            var inventory = new Inventory();
+            dataGridView1.DataSource = inventory.displayInventory(dataGridView1);
+            dataGridView1.Columns[0].HeaderText = "Item Name";
+            dataGridView1.Columns[1].HeaderText = "ID";
+            dataGridView1.Columns[2].HeaderText = "Purchase Date";
+            dataGridView1.Columns[3].HeaderText = "Replacement Date";
+            dataGridView1.Columns[0].HeaderCell.Style.Alignment = DataGridViewContentAlignment.MiddleCenter;
+            dataGridView1.Columns[1].HeaderCell.Style.Alignment = DataGridViewContentAlignment.MiddleCenter;
+            dataGridView1.Columns[2].HeaderCell.Style.Alignment = DataGridViewContentAlignment.MiddleCenter;
+            dataGridView1.Columns[3].HeaderCell.Style.Alignment = DataGridViewContentAlignment.MiddleCenter;
+        }
+
+        private void inventoryUpdateButton_Click(object sender, EventArgs e)
+        {
+            if (isDuplicates() == true)
+            {
+                MessageBox.Show("Error: There is a duplicate ID in inventory table. Fix the ID before updating your inventory.");
+            }
+            else
+            {
+                Database db = new Database();
+                SqlCommand command = new SqlCommand("Select * From Inventory", db.con);
+                DataSet oldTable = db.sendSelectCommand(command);
+                int i = 0;
+                while (i < oldTable.Tables[0].Rows.Count)
+                {
+                    if (containsID(dataGridView1, oldTable.Tables[0].Rows[i][1].ToString()) == false)
+                    {
+                        command.CommandText = "Delete From Inventory Where id=@oldID";
+                        command.Parameters.Clear();
+                        command.Parameters.AddWithValue("@oldID", oldTable.Tables[0].Rows[i][1]);
+                        db.sendDeleteCommand(command);
+                    }
+                    i++;
+                }
+                i = 0;
+                while (i < dataGridView1.Rows.Count - 1)
+                {
+                    if (containsID(oldTable, dataGridView1.Rows[i].Cells[1]) == true)
+                    {
+                        command.CommandText = "Update Inventory Set item=@item,id=@id,purchaseDate=@purchaseDate,replacementDate=@replacementDate Where id=@updateID";
+                        command.Parameters.Clear();
+                        command.Parameters.AddWithValue("@item", dataGridView1.Rows[i].Cells[0].Value.ToString());
+                        command.Parameters.AddWithValue("@id", dataGridView1.Rows[i].Cells[1].Value.ToString());
+                        command.Parameters.AddWithValue("@purchaseDate", dataGridView1.Rows[i].Cells[2].Value.ToString());
+                        command.Parameters.AddWithValue("@replacementDate", dataGridView1.Rows[i].Cells[3].Value.ToString());
+                        command.Parameters.AddWithValue("@updateID", dataGridView1.Rows[i].Cells[1].Value.ToString());
+                        db.sendUpdateCommand(command);
+                    }
+                    else if (containsID(oldTable, dataGridView1.Rows[i].Cells[1]) == false)
+                    {
+                        Inventory inventory = new Inventory();
+                        inventory.insertInventoryItem(dataGridView1.Rows[i].Cells[0].Value.ToString(), Convert.ToInt32(dataGridView1.Rows[i].Cells[1].Value.ToString()), Convert.ToDateTime(dataGridView1.Rows[i].Cells[2].Value.ToString()), Convert.ToDateTime(dataGridView1.Rows[i].Cells[3].Value.ToString()));
+                    }
+                    i++;
+                }
+            }
+        }
+        private bool isDuplicates()
+        {
+            List<int> masterList = new List<int>();
+            for (int i = 0; i < dataGridView1.Rows.Count - 1; i++)
+            {
+                if (masterList.Contains(Convert.ToInt32(dataGridView1.Rows[i].Cells[1].Value.ToString()))){
+                    return true;
+                }
+                else
+                {
+                    masterList.Add(Convert.ToInt32(dataGridView1.Rows[i].Cells[1].Value.ToString()));
+                }
+            }
+            return false;
+        }
+        private bool containsID(DataSet ds, DataGridViewCell c)
+        {
+            int i = 0;
+            while(i < ds.Tables[0].Rows.Count)
+            {
+                if(ds.Tables[0].Rows[i][1].ToString() == c.Value.ToString())
+                {
+                    return true;
+                }
+                i++;
+            }
+            return false;
+        }
+        private bool containsID(DataGridView table, String c)
+        {
+            int i = 0;
+            while (i < table.Rows.Count-1)
+            {
+                if(table.Rows[i].Cells[1].Value.ToString() == c.ToString())
+                {
+                    return true;
+                }
+                i++;
+            }
+            return false;
+        }
+    }
+}
