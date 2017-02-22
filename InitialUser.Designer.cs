b8a4d18c4e9e77fec0572adc61c4dea629847944
Drake <drake.olson@jacks.sdstate.edu>
2017-02-21 22:01:37 -0600

    Added Inital User Setup, Resolution Changed to Better Fit 720p

---
+++ InitialUser.Designer.cs
@@ -0,0 +1,133 @@
+﻿namespace SDSU_Rock_Wall_CRM
+{
+    partial class InitialUser
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.addManager = new System.Windows.Forms.RadioButton();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.desiredUserNameLabel = new System.Windows.Forms.Label();
+            this.passwordLabel = new System.Windows.Forms.Label();
+            this.userNameTextBox = new System.Windows.Forms.TextBox();
+            this.passwordTextBox = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // addManager
+            // 
+            this.addManager.AutoSize = true;
+            this.addManager.Location = new System.Drawing.Point(13, 46);
+            this.addManager.Name = "addManager";
+            this.addManager.Size = new System.Drawing.Size(89, 17);
+            this.addManager.TabIndex = 0;
+            this.addManager.TabStop = true;
+            this.addManager.Text = "Add Manager";
+            this.addManager.UseVisualStyleBackColor = true;
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.Location = new System.Drawing.Point(13, 86);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(85, 17);
+            this.radioButton2.TabIndex = 0;
+            this.radioButton2.TabStop = true;
+            this.radioButton2.Text = "Add Master";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            // 
+            // desiredUserNameLabel
+            // 
+            this.desiredUserNameLabel.AutoSize = true;
+            this.desiredUserNameLabel.Location = new System.Drawing.Point(108, 131);
+            this.desiredUserNameLabel.Name = "desiredUserNameLabel";
+            this.desiredUserNameLabel.Size = new System.Drawing.Size(97, 13);
+            this.desiredUserNameLabel.TabIndex = 1;
+            this.desiredUserNameLabel.Text = "Desired Username:";
+            // 
+            // passwordLabel
+            // 
+            this.passwordLabel.AutoSize = true;
+            this.passwordLabel.Location = new System.Drawing.Point(149, 171);
+            this.passwordLabel.Name = "passwordLabel";
+            this.passwordLabel.Size = new System.Drawing.Size(56, 13);
+            this.passwordLabel.TabIndex = 2;
+            this.passwordLabel.Text = "Password:";
+            // 
+            // userNameTextBox
+            // 
+            this.userNameTextBox.Location = new System.Drawing.Point(211, 128);
+            this.userNameTextBox.Name = "userNameTextBox";
+            this.userNameTextBox.Size = new System.Drawing.Size(117, 20);
+            this.userNameTextBox.TabIndex = 3;
+            // 
+            // passwordTextBox
+            // 
+            this.passwordTextBox.Location = new System.Drawing.Point(211, 168);
+            this.passwordTextBox.Name = "passwordTextBox";
+            this.passwordTextBox.Size = new System.Drawing.Size(117, 20);
+            this.passwordTextBox.TabIndex = 4;
+            this.passwordTextBox.UseSystemPasswordChar = true;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(253, 208);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 5;
+            this.button1.Text = "Submit";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // InitialUser
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(522, 374);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.passwordTextBox);
+            this.Controls.Add(this.userNameTextBox);
+            this.Controls.Add(this.passwordLabel);
+            this.Controls.Add(this.desiredUserNameLabel);
+            this.Controls.Add(this.radioButton2);
+            this.Controls.Add(this.addManager);
+            this.Name = "InitialUser";
+            this.Text = "InitialUser";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RadioButton addManager;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.Label desiredUserNameLabel;
+        private System.Windows.Forms.Label passwordLabel;
+        private System.Windows.Forms.TextBox userNameTextBox;
+        private System.Windows.Forms.TextBox passwordTextBox;
+        private System.Windows.Forms.Button button1;
+    }
+}
---
+++ InitialUser.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.Sql;
+using System.Data.SqlClient;
+
+namespace SDSU_Rock_Wall_CRM
+{
+    public partial class InitialUser : Form
+    {
+        public InitialUser()
+        {
+            InitializeComponent();
+        }
+
+        private void radioButton2_CheckedChanged(object sender, EventArgs e)
+        {
+            this.addManager.Checked = false;
+        }
+
+        private void addManager_CheckedChanged(object sender, EventArgs e)
+        {
+            this.radioButton2.Checked = false;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (this.radioButton2.Checked == true)
+            {
+                var db = new Database();
+                SqlCommand checkDuplicate = new SqlCommand("Select * from Masters where username=@username", db.con);
+                checkDuplicate.Parameters.AddWithValue("@username", this.userNameTextBox.Text.ToString());
+                DataSet userDuplicate = db.sendSelectCommand(checkDuplicate);
+                if (userDuplicate.Tables[0].Rows.Count > 0)
+                {
+                    this.userNameTextBox.Text = "";
+                    this.passwordTextBox.Text = "";
+                    MessageBox.Show("Error: Username already exists.");
+                }
+                else
+                {
+                    SqlCommand command = new SqlCommand("Insert Into Masters Values (@username,@password)", db.con);
+                    var hashedPassword = loginForm.hashPassword(passwordTextBox.Text.ToString());
+                    command.Parameters.AddWithValue("@username", this.userNameTextBox.Text.ToString());
+                    command.Parameters.AddWithValue("@password", hashedPassword);
+                    db.sendInsertCommand(command);
+                    MessageBox.Show("Inserted Account Successfully!");
+                }
+            }
+            else
+            {
+                var db = new Database();
+                SqlCommand checkDuplicate = new SqlCommand("Select * from Admins where username=@username", db.con);
+                checkDuplicate.Parameters.AddWithValue("@username", this.userNameTextBox.Text.ToString());
+                DataSet userDuplicate = db.sendSelectCommand(checkDuplicate);
+                if (userDuplicate.Tables[0].Rows.Count > 0)
+                {
+                    this.userNameTextBox.Text = "";
+                    this.passwordTextBox.Text = "";
+                    MessageBox.Show("Error: Username already exists.");
+                }
+                else
+                {
+                    SqlCommand command = new SqlCommand("Insert Into Admins Values (@username,@password)", db.con);
+                    var hashedPassword = loginForm.hashPassword(passwordTextBox.Text.ToString());
+                    command.Parameters.AddWithValue("@username", this.userNameTextBox.Text.ToString());
+                    command.Parameters.AddWithValue("@password", hashedPassword);
+                    db.sendInsertCommand(command);
+                    MessageBox.Show("Inserted Account Successfully!");
+                }
+            }
+            this.userNameTextBox.Text = "";
+            this.passwordTextBox.Text = "";
+        }
+    }
+}
