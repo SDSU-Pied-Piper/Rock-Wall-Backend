3fe62a86885fcd1a3198dcad47d5c5ebf268118a
Drake <drake.olson@jacks.sdstate.edu>
2017-02-28 20:28:33 -0600

    Master Report: Iventory Working still needs testing

--- Report.cs
+++ Report.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text;
 using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using ClosedXML.Excel;
@@ -19,7 +20,12 @@ namespace SDSU_Rock_Wall_CRM
             workSheets.Add(workBook.Worksheets.Add("User Information"));
             workSheets.Add(workBook.Worksheets.Add("Current Suspensions"));
             workSheets.Add(workBook.Worksheets.Add("Inventory"));
+            workSheets.Add(workBook.Worksheets.Add("Certified Users"));
             initialSetUp(workBook, workSheets);
+            inventorySetUp(workBook, workSheets);
+            suspensionsSetUp(workBook, workSheets);
+            userInformationSetUp(workBook, workSheets);
+            certifiedUserSetUp(workBook, workSheets);
             String fileName = String.Format("Master Report {0} {1}", DateTime.Now.ToShortDateString(), DateTime.Now.ToShortTimeString());
             Regex rgx = new Regex("[^a-zA-Z0-9 -]");
             fileName = rgx.Replace(fileName, "");
@@ -74,6 +80,88 @@ namespace SDSU_Rock_Wall_CRM
             workSheets[2].Cell("D1").Value = "Item Replacement Date";
             workSheets[2].Cell("D1").Style.Font.Bold = true;
             workSheets[2].Columns().AdjustToContents();
+
+            workSheets[3].Cell("A1").Value = "First Name";
+            workSheets[3].Cell("A1").Style.Font.Bold = true;
+            workSheets[3].Cell("B1").Value = "Last Name";
+            workSheets[3].Cell("B1").Style.Font.Bold = true;
+            workSheets[3].Cell("C1").Value = "Student ID";
+            workSheets[3].Cell("C1").Style.Font.Bold = true;
+            workSheets[3].Cell("D1").Value = "Gender";
+            workSheets[3].Cell("D1").Style.Font.Bold = true;
+            workSheets[3].Cell("E1").Value = "Belay Certified";
+            workSheets[3].Cell("E1").Style.Font.Bold = true;
+            workSheets[3].Cell("F1").Value = "Lead Certified";
+            workSheets[3].Cell("F1").Style.Font.Bold = true;
+            workSheets[3].Columns().AdjustToContents();
+        }
+        private void inventorySetUp(XLWorkbook workbook, List<IXLWorksheet> workSheets)
+        {
+            Database db = new Database();
+            SqlCommand command = new SqlCommand("Select * from Inventory", db.con);
+            DataSet inventory = db.sendSelectCommand(command);
+            StringBuilder cell = new StringBuilder("A2");
+            for(int i = 0; i < inventory.Tables[0].Rows.Count; i++)
+            {
+                for (int j = 0; j < inventory.Tables[0].Columns.Count; j++)
+                {
+                    workSheets[2].Cell(cell.ToString()).Value = inventory.Tables[0].Rows[i][j];
+                    cell[0] = (char)((int)(cell[0]) + 1);
+                }
+                cell[0] = 'A';
+                cell[1] = (char)((int)(cell[1]) + 1);
+            }
+        }
+        private void suspensionsSetUp(XLWorkbook workbook, List<IXLWorksheet> workSheets)
+        {
+            Database db = new Database();
+            SqlCommand command = new SqlCommand("Select firstName,lastName,studentID,dateOfBirth,gender,dateSuspended,dateUnsuspended from Patrons where isSuspended=1", db.con);
+            DataSet suspensions = db.sendSelectCommand(command);
+            StringBuilder cell = new StringBuilder("A2");
+            for (int i = 0; i < suspensions.Tables[0].Rows.Count; i++)
+            {
+                for (int j = 0; j < suspensions.Tables[0].Columns.Count; j++)
+                {
+                    workSheets[2].Cell(cell.ToString()).Value = suspensions.Tables[0].Rows[i][j];
+                    cell[0] = (char)((int)(cell[0]) + 1);
+                }
+                cell[0] = 'A';
+                cell[1] = (char)((int)(cell[1]) + 1);
+            }
+        }
+        private void userInformationSetUp(XLWorkbook workbook, List<IXLWorksheet> workSheets)
+        {
+            Database db = new Database();
+            SqlCommand command = new SqlCommand("Select firstName,lastName,studentID,dateOfBirth,gender,isSuspended,isBlayCertified,isLeadCertified,lastCheckIn,mailingList from Patrons where isWaiverSigned=1", db.con);
+            DataSet users = db.sendSelectCommand(command);
+            StringBuilder cell = new StringBuilder("A2");
+            for (int i = 0; i < users.Tables[0].Rows.Count; i++)
+            {
+                for (int j = 0; j < users.Tables[0].Columns.Count; j++)
+                {
+                    workSheets[2].Cell(cell.ToString()).Value = users.Tables[0].Rows[i][j];
+                    cell[0] = (char)((int)(cell[0]) + 1);
+                }
+                cell[0] = 'A';
+                cell[1] = (char)((int)(cell[1]) + 1);
+            }
+        }
+        private void certifiedUserSetUp(XLWorkbook workbook, List<IXLWorksheet> workSheets)
+        {
+            Database db = new Database();
+            SqlCommand command = new SqlCommand("Select firstName,lastName,studentID,dateOfBirth,gender,isBlayCertified,isLeadCertified,lastCheckIn,numberofCheckIns,mailingList from Patrons where isWaiverSigned=1", db.con);
+            DataSet suspensions = db.sendSelectCommand(command);
+            StringBuilder cell = new StringBuilder("A2");
+            for (int i = 0; i < suspensions.Tables[0].Rows.Count; i++)
+            {
+                for (int j = 0; j < suspensions.Tables[0].Columns.Count; j++)
+                {
+                    workSheets[2].Cell(cell.ToString()).Value = suspensions.Tables[0].Rows[i][j];
+                    cell[0] = (char)((int)(cell[0]) + 1);
+                }
+                cell[0] = 'A';
+                cell[1] = (char)((int)(cell[1]) + 1);
+            }
         }
     }
 }
